12d44d7052d712b4fec1bd5a025afb2b71727a56
estri012 <estri.puji.lestari@mail.ugm.ac.id>
2023-05-03 15:41:01 +0700

    views

---
+++ resources/views/index.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+        <link rel="stylesheet" href="//cdn.datatables.net/1.10.20/css/jquery.dataTables.min.css">
+        <title>Input Data Harian</title>
+    </head>
+
+
+<body>
+    <div class="container"></div>
+</body>
+</html>
